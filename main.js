36b039adc87d556d84ea932a65383dfe675ca424
AbdulbositAwurov <abdulbositashurov497@gmail.com>
2023-02-02 09:03:22 +0300

    initial.commit

---
+++ main.js
@@ -0,0 +1,48 @@
+
+
+let Myname = document.getElementById('btn');
+
+let rang = prompt('Button ranginni kiriting');
+
+Myname.style.color = rang
+
+let   = document.getElementById('btn');
+
+let width = prompt('widthni kiriting')
+
+Myname.style.width = width
+
+
+
+let   = document.getElementById('btn');
+let height = prompt('heightni kiriting');
+
+Myname.style.height = height
+
+let   = document.getElementById('btn');
+let border = prompt('border kriting')
+
+Myname.style.border = border
+
+let   = document.getElementById('btn');
+
+let background = prompt('background color kriting')
+
+Myname.style.backgroundColor = background
+
+let   = document.getElementById('btn');
+
+let fontsize = prompt('fontsize kriting')
+Myname.style.fontsize = fontsize
+
+let   = document.getElementById('btn');
+
+let fontweight = prompt('fontweight kriting')
+Myname.style.fontWeight = fontweight
+
+let   = document.getElementById('btn');
+
+let fontfamily = prompt('fontfamily kriting')
+Myname.style.fontFamily = fontfamily
+
+
